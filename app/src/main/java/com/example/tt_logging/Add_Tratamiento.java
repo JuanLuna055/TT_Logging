15820fc1a1eea3df62e205362dde71feed0d5129
aldoe <aldoefer_@hotmail.com>
2020-12-17 21:59:56 -0600

    Agregar los tratamientos y las imagenes en firebase

---
+++ app/src/main/java/com/example/tt_logging/Add_Tratamiento.java
@@ -0,0 +1,150 @@
+package com.example.tt_logging;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import id.zelory.compressor.Compressor;
+
+public class Add_Tratamiento extends AppCompatActivity {
+    Button subir,seleccionar;
+    ImageView foto;
+    DatabaseReference imgref;
+    StorageReference storageReference;
+    ProgressDialog cargando;
+
+    Bitmap thumb_bitmap = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__tratamiento);
+        foto=findViewById(R.id.img_foto);
+        seleccionar=findViewById(R.id.btn_selefoto);
+        subir=findViewById(R.id.btn_subirfoto);
+        imgref= FirebaseDatabase.getInstance().getReference().child("Fotos_Subidas");
+        storageReference = FirebaseStorage.getInstance().getReference().child("img_comprimidas");
+        cargando = new ProgressDialog(this);
+
+        seleccionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.startPickImageActivity(Add_Tratamiento.this);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK)
+        {
+            Uri imageuri =CropImage.getPickImageResultUri(this,data);
+
+            //Recortar Imagen
+            CropImage.activity(imageuri).setGuidelines(CropImageView.Guidelines.ON).setRequestedSize(640,480)
+                    .setAspectRatio(2,1).start(Add_Tratamiento.this);
+        }
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode==RESULT_OK)
+            {
+                Uri resultUri = result.getUri();
+                File url = new File(resultUri.getPath());
+                Picasso.with(this).load(url).into(foto);
+                //Comprimiendo Imagen
+
+                try{
+                    thumb_bitmap = new Compressor(this)
+                            .setMaxWidth(640).setMaxHeight(480)
+                            .setQuality(90).compressToBitmap(url);
+                }catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                thumb_bitmap.compress(Bitmap.CompressFormat.JPEG,90,byteArrayOutputStream);
+                final byte [] thumb_byte = byteArrayOutputStream.toByteArray();
+
+                //fin del compresor...
+
+                int p=(int) (Math.random() * 8+1);
+                int s=(int) (Math.random() * 8+1);
+                int  c=(int) (Math.random() * 8+1);
+                int numero1=(int) (Math.random() * 1012+2111);
+                int numero2=(int) (Math.random() * 1012+2111);
+
+                String[] elementos = {"a","b","c","d","e","f","g","h","i"};
+
+                final String aleatorio = elementos[p]+elementos[s]+numero1+elementos[c]+numero2+"comprimido.jpg";
+
+                subir.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        cargando.setTitle("Subiendo foto...");
+                        cargando.setMessage("Espere por favor..");
+                        cargando.show();
+
+                        final StorageReference ref = storageReference.child(aleatorio);
+                        UploadTask uploadTask = ref.putBytes(thumb_byte);
+
+                        //Subir imagen en Storage
+                        Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                            @Override
+                            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                if(!task.isSuccessful()){
+                                    throw Objects.requireNonNull(task.getException());
+                                }
+                                return ref.getDownloadUrl();
+                            }
+                        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                Uri downloaduri=task.getResult();
+                                imgref.push().child("urlfoto").setValue(downloaduri.toString());
+                                cargando.dismiss();
+
+                                Toast.makeText(Add_Tratamiento.this,"Imagen cargada con exito",Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+                    }
+                });
+
+
+            }
+        }
+    }
+}
